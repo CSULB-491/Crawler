9d0b67d0e916b2edd6a0b93da42e22faf40ed868
Ian Ward <golfinian@gmail.com>
2020-02-03 22:31:34 -0800

    parses a single rss feed
    if you want to see it in action open the terminal and run:
     - python manage.py shell
     - from crawler.views import Crawler
     - Crawler()
    The results print to the console for now

--- project_validator/crawler/models.py
+++ project_validator/crawler/models.py
@@ -25,3 +25,8 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class RSSFeed(models.Model):
+    address = models.CharField(max_length=200)
+    publisher = models.ForeignKey(Publisher, on_delete=models.SET_NULL, null=True)
--- project_validator/crawler/views.py
+++ project_validator/crawler/views.py
@@ -3,19 +3,75 @@ import feedparser
 from .models import Author
 from .models import Publisher
 from .models import Article
+from datetime import datetime
+
+
 # Create your views here.
 
 
 class Crawler:
+    banned_sites = []
+    possible_article_attributes = ['title', 'author', 'author_detail', 'publisher', 'publisher_detail',
+                                   'contributors', 'tags', 'link', 'published', 'description', 'content', 'summary']
+    rss_feeds = ['http://feeds.bbci.co.uk/news/world/rss.xml',
+                 'http://feeds.reuters.com/Reuters/worldNews', 'http://feeds.washingtonpost.com/rss/rss_blogpost',
+                 'https://www.yahoo.com/news/rss/world', 'http://rss.cnn.com/rss/edition_world.rss',
+                 'http://rssfeeds.usatoday.com/usatoday-newstopstories&x=1', 'https://www.yahoo.com/news/rss/',
+                 'http://feeds.reuters.com/Reuters/domesticNews', 'http://feeds.skynews.com/feeds/rss/us.xml',
+                 'http://rss.cnn.com/rss/edition_us.rss', 'http://feeds.skynews.com/feeds/rss/uk.xml',
+                 'http://feeds.bbci.co.uk/news/rss.xml', 'http://feeds.reuters.com/reuters/UKdomesticNews',
+                 'https://www.theguardian.com/uk/rss', 'https://techcrunch.com/rssfeeds/',
+                 'http://rss.slashdot.org/Slashdot/slashdot', "https://news.google.com/news/rss",
+                 'https://spectrum.ieee.org/rss/blog/tech-talk/fulltext', 'https://www.techworld.com/news/rss',
+                 'https://www.wired.com/feed',
+                 'http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml',
+                 'https://www.npr.org/rss/rss.php?id=1045',
+                 'https://www.fandango.com/rss/newmovies.rss', 'http://www.metacritic.com/rss/movies',
+                 'https://www.rogerebert.com/feed',
+                 'http://www.movies.com/rss-feeds/movie-news-rss', 'http://www.si.com/rss/si_topstories.rss',
+                 'http://feeds1.nytimes.com/nyt/rss/Sports', 'https://talksport.com/rss/sports-news/all/feed',
+                 'http://feeds.sport24.co.za/articles/Sport/Featured/TopStories/rss',
+                 'http://rss.cnn.com/rss/edition_sport.rss',
+                 'http://syndication.eonline.com/syndication/feeds/rssfeeds/topstories.xml',
+                 'http://feeds.reuters.com/reuters/entertainment',
+                 'http://www.instyle.com/feeds/all/ins.rss',
+                 'http://feeds.accesshollywood.com/AccessHollywood/LatestNews',
+                 'https://www.npr.org/rss/rss.php?id=1008', 'https://api.quantamagazine.org/feed/']
+
     def __init__(self):
         self.parse()
-        self.create_author("test", "author")
+
+    @classmethod
+    def gather_article_attributes(cls, article):
+        current_article = {}
+        for attribute in cls.possible_article_attributes:
+            if hasattr(article, attribute):
+                current_article[attribute] = article[attribute] if article[attribute] else "null"
+
+        return current_article
 
     @classmethod
     def parse(cls):
-        parsed_feed = feedparser.parse("http://www.reddit.com/r/python/.rss")
-        print(parsed_feed['feed']['title'])
+        # @TODO need to create a pre-parse hash that will identify if article already has been processed
+        articles = feedparser.parse(cls.rss_feeds[0])
+        # articles = feedparser.parse('https://api.quantamagazine.org/feed/')
 
+        for article in articles.entries:
+            article_attributes = cls.gather_article_attributes(article)
+
+            # can add these in later if so inclined
+            # logo = article.feed.logo if article.feed.logo else "No logo"
+            # image = article.feed.image if article.feed.image else "No image"
+
+            try:
+                print(article_attributes)
+            except:
+                print("couldn't print an encoded character")
+
+            # @TODO need to follow links to get the full context if context not found
+            # @TODO decide what nulls are going to look like, be careful with "null" currently inplace
+            # @TODO create a textfile with all the parsed content and then create a db entry
+            # @TODO descriptions and summaries appear to be the same thing
     @classmethod
     def crawl_the_web(cls, time_interval):
         print("crawler is crawling")
@@ -26,6 +82,8 @@ class Crawler:
         author = Author()
         author.first_name = first_name
         author.last_name = last_name
+        author.save()
+        cls.print_authors()
 
     @classmethod
     def create_publisher(cls, name):
@@ -41,13 +99,26 @@ class Crawler:
 
     @classmethod
     def add_rss_feed(cls, url):
+        cls.rss_feeds.append(url)
         print("adding feed")
 
     @classmethod
-    def edit_rss_feed(cls, feed):
-        print("editing feed")
+    def edit_rss_feed(cls, current_rss_feed, edited_rss_feed):
+        index = cls.rss_feeds.index(current_rss_feed)
+        cls.rss_feeds[index] = edited_rss_feed
+        print("edited feed")
 
     @classmethod
     def remove_rss_feed(cls, feed):
+        cls.rss_feeds.remove(feed)
         print("removing feed")
 
+    @classmethod
+    def filter_site(cls, url):
+        print("filtering")
+
+    @classmethod
+    def print_authors(cls):
+        authors = Author.objects.all()
+        for author in authors:
+            print(author.first_name, author.last_name)
