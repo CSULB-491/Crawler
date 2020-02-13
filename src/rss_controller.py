15d38c2bbd7100f378db30f51b47fc52dcdbb787
Ian Ward <golfinian@gmail.com>
2020-02-12 16:31:58 -0800

    Setup Title Tokenizer

--- src/rss_controller.py
+++ src/rss_controller.py
@@ -1,10 +1,11 @@
 import feedparser
 from src.html_controller import HTMLParser
+# from textblob import TextBlob
 
 
 class Crawler:
     banned_sites = []
-    rss_attributes = ['title', 'author', 'publisher', 'contributors', 'tags', 'link', 'published']
+    rss_attributes = ['title', 'author', 'publisher', 'contributors', 'link', 'published', "tokenizedTitle"]
     rss_feeds = ['http://feeds.bbci.co.uk/news/world/rss.xml',
                  'http://feeds.reuters.com/Reuters/worldNews', 'http://feeds.washingtonpost.com/rss/rss_blogpost',
                  'https://www.yahoo.com/news/rss/world', 'http://rss.cnn.com/rss/edition_world.rss',
@@ -32,6 +33,7 @@ class Crawler:
 
     def __init__(self):
         self.update_rss_feeds()
+        # self.tokenTest()
 
     # Takes an rss feed and returns a dict containing its attributes(Author, publisher, date...)
     def parse_rss_feed(self, rss_feed: feedparser.FeedParserDict) -> dict:
@@ -58,6 +60,7 @@ class Crawler:
             # can add these in later if so inclined
             # logo = article.feed.logo if article.feed.logo else "No logo"
             # image = article.feed.image if article.feed.image else "No image"
+            # self.tokenizeTitle(article_attributes)
 
             try:
                 print(article_attributes, "\n")  # If you are printing \u2019 and such run <chcp 65001> in the terminal
@@ -65,18 +68,21 @@ class Crawler:
                 print(str(e))
 
             # @TODO decide what nulls are going to look like for article attributes, be careful with "null"
-            # @TODO create a textfile with all the parsed content and then create a db entry
-            # @TODO create a reliable hash to store in the db so rss_source can be quickly checked for existence
+            # @TODO pass content to nlp as a string, pass parsed title to nlp as list
+            # @TODO Richard - create a reliable hash to store in the db so rss_source can be quickly checked for existence (name, date, publisher)
             # @TODO need to make crawling asynchronous
+            # @TODO Tokenize/clean the title
             # @TODO remove tags with :None
-            # @TODO handle 404 errors gracefully in html crawler + add timeouts
+            # @TODO Richard - handle 404 errors gracefully in html crawler + add timeouts
             # @TODO put everything in a loop that won't exceed the nlp's money constraints
             # @TODO create all the authors and such as parsed
             # @TODO decide on how to separate authors with same name/ identify those who write for 2+ publishers
             # @TODO \xa0 still shows up
+            # @TODO \parse author names (break them up and remove '&, and, by'...etc
             # @TODO Create a manager aspect that handles articles rated by NLP and user interactions with articles?
-            # @TODO Decide how to sanitize user entered rss feeds
-            # @TODO Handle site bans
+
+    def tokenizeTitle(self, article_attributes: dict):
+        print("tokenizing title")
 
 
 Crawler()
