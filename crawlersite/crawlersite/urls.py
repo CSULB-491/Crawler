58044fd3d11f38e2c3917bc529b72b4d5e65b2b2
richytort <47453583+richytort@users.noreply.github.com>
2020-03-03 08:27:40 -0800

    Test for other comp

--- crawlersite/crawlersite/urls.py
+++ crawlersite/crawlersite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 
+
 urlpatterns = [
     url(r'^publisher/', include('publisher.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+######################## TEST CHANGE ##################################
